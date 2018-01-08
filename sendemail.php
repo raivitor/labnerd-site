d201f645d8513872577ee063dbb4f236ac14d071
Rai Vitor <raivitor@gmail.com>
2018-01-08 10:41:00 -0300

    Atualizando script de email 2.0

--- sendemail.php
+++ sendemail.php
@@ -1,16 +1,23 @@
 <?php
-  $name = $_POST['name'];
-  $email = $_POST['email'];
   $subject = $_POST['subject'];
-  $message = $_POST['message'];
+  $emailsender = "contato@labnerd.com.br";
+  if(PHP_OS == "Linux") $quebra_linha = "\n"; //Se for Linux
+  elseif(PHP_OS == "WINNT") $quebra_linha = "\r\n"; // Se for Windows
+  else die("Este script nao esta preparado para funcionar com o sistema operacional de seu servidor");
+  $emailremetente    = trim($_POST['email']);
+  $emaildestinatario = $emailsender;
+  $mensagem          = trim($_POST['message']); 
+  $mensagemHTML = '<p>Nome: '. $_POST['name'].'</p><p> Email: '.$emailremetente.'<p>Mensagem: <b><i>'.$mensagem.'</i></b></p><hr>'; 
+  $headers = "MIME-Version: 1.1".$quebra_linha;
+  $headers .= "Content-type: text/html; charset=utf-8".$quebra_linha;
+  $headers .= "From: ".$emailremetente.$quebra_linha;
+  $headers .= "Return-Path: " . $emailremetente . $quebra_linha;
+  $headers .= "Reply-To: ".$emailremetente.$quebra_linha;
 
-  $from = $email;
-  $to = "contato@labnerd.com.br";
-  $headers = "De:". $from;
-  if(mail($to, $subject, $message, $headers)){
-    echo 1;
-  } else {
-    echo 0;
+  if(mail($emaildestinatario, $subject, $mensagemHTML, $headers, "-r". $emailsender)){
+      echo 1;
   }
-  
+  else{
+      echo 0;
+  }
 ?>
